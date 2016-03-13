ffa311cfb7040d6d226e8c8018c7014dc1ce24fc
matrixsmaster <matrixsmaster@yandex.ru>
2016-03-13 23:10:45 +0400

    Stack cache implemented

--- pzpu/ram.c
+++ pzpu/ram.c
@@ -52,22 +52,72 @@ inline static void ram_icache_sync()
 
 inline static uint8_t ram_icache(uint32_t adr)
 {
-//	msg(0,"B %u L %hhu A %u\n",icache_base,icache_vlen,adr);
 	if ((adr < icache_base) || (adr >= icache_base + icache_vlen)) {
 		//ram_icache_sync();
 		icache_base = adr;
 		icache_vlen = ram_rd_seq(adr,RAM_ICACHE,icache_data);
 
 		if (!icache_vlen) return 0;
-//		msg(0,"IC 0x"PFMT_32XINT" new (vlen = "PFMT_8UINT")\n",adr,icache_vlen);
 		return icache_data[0];
 	}
-//	msg(0,"IC 0x"PFMT_32XINT" hit (off = "PFMT_32UINT")\n",adr,adr-icache_base);
 	return icache_data[adr-icache_base];
 }
 #endif /* ICACHE */
 
 #if RAM_SCACHE
+#define RAM_SGAP (((RAM_SCACHE - 1) / 2) * 4)
+static uint32_t scache_base = 0;
+static uint32_t scache_data[RAM_SCACHE];
+static uint16_t scache_vlen = 0;
+
+inline static void ram_scache_sync()
+{
+	if (scache_vlen) //there's something to write
+		ram_wr_seq(scache_base,scache_vlen,(uint8_t*)scache_data);
+}
+
+inline static uint8_t ram_scache_check(uint32_t adr)
+{
+	if ((adr < scache_base) || (adr >= scache_base + scache_vlen)) {
+		ram_scache_sync();
+		if (adr < RAM_SGAP) scache_base = 0; //wtf? we're at the memory start point
+		else scache_base = (adr & (~3)) - RAM_SGAP;
+		scache_vlen = ram_rd_seq(scache_base,RAM_SCACHE*4,(uint8_t*)scache_data);
+
+		if (scache_vlen & 3) {
+			scache_vlen = scache_vlen & (~3);
+			if (!scache_vlen) {
+#ifdef RAM_DBG
+				msg(1,"SCache: Vlen = "PFMT_16UINT"\n",scache_vlen);
+#endif
+#ifdef RAM_OUT_ABORT
+				abort();
+#endif
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+inline static uint32_t ram_scache_r(uint32_t adr)
+{
+	if (!ram_scache_check(adr)) return 0;
+	return scache_data[(adr - scache_base)/4];
+}
+
+inline static void ram_scache_w(uint32_t adr, uint32_t val)
+{
+	if (!ram_scache_check(adr)) return;
+	scache_data[(adr - scache_base)/4] = val;
+}
+
+static void ram_scache_init(uint32_t sp)
+{
+	//just read without sync
+	scache_vlen = 0;
+	ram_scache_r(sp);
+}
 #endif /* SCACHE */
 
 #if EMBED_AVR
@@ -90,6 +140,7 @@ int ram_init(uint32_t sz)
 #endif
 
 #if RAM_ICACHE
+	ram_icache_init();
 #endif
 
 	return 0;
@@ -110,6 +161,9 @@ void ram_release()
 
 uint8_t ram_rd_b(uint32_t adr)
 {
+#if RAM_ICACHE
+	return ram_icache(adr);
+#else
 	uint8_t r = 0;
 	if (adr >= ramsize) {
 #ifdef RAM_DBG
@@ -124,8 +178,10 @@ uint8_t ram_rd_b(uint32_t adr)
 	msg(1,"Unable to read [B] (adr = 0x"PFMT_32XINT")\n",adr);
 #endif
 	return 0;
+#endif /* ICACHE */
 }
 
+/*
 void ram_wr_b(uint32_t adr, uint8_t val)
 {
 	if (adr >= ramsize) {
@@ -136,6 +192,7 @@ void ram_wr_b(uint32_t adr, uint8_t val)
 	}
 	sd_raw_read(img_offset+adr,&val,1);
 }
+*/
 
 uint32_t ram_rd_dw(uint32_t adr)
 {
@@ -169,6 +226,8 @@ void ram_wr_dw(uint32_t adr, uint32_t val)
 
 #else /* HOST = PC (x86) */
 
+#include <byteswap.h> //for endianness conversion
+
 int ram_init(uint32_t sz)
 {
 	if (RAM) ram_release();
@@ -184,8 +243,8 @@ int ram_init(uint32_t sz)
 #if RAM_ICACHE
 	ram_icache_init();
 #endif
-
 #if RAM_SCACHE
+	ram_scache_init(sz-4);
 #endif
 
 	return 0;
@@ -193,14 +252,17 @@ int ram_init(uint32_t sz)
 
 void ram_release()
 {
+#if RAM_ICACHE
+	//ram_icache_sync();
+#endif
+#if RAM_SCACHE
+	ram_scache_sync();
+#endif
+
 	if (!RAM) return;
 	free(RAM);
 	ramsize = 0;
 	RAM = NULL;
-
-#if RAM_ICACHE
-	//ram_icache_sync();
-#endif
 
 #ifdef RAM_DBG
 	msg(0,"RAM released.\n");
@@ -243,6 +305,9 @@ int ram_load(const char* fn, uint32_t maxsz)
 #if RAM_ICACHE
 	ram_icache_init();
 #endif
+#if RAM_SCACHE
+	ram_scache_init(sz-4);
+#endif
 
 	return 0;
 }
@@ -250,6 +315,9 @@ int ram_load(const char* fn, uint32_t maxsz)
 
 void ram_wr_dw(uint32_t adr, uint32_t val)
 {
+#if RAM_SCACHE
+	ram_scache_w(adr,__bswap_32(val));
+#else
 	if (adr >= ramsize-3) {
 #ifdef RAM_DBG
 		msg(1,"Reached out of memory [WR DW] (adr = 0x"PFMT_32XINT")\n",adr);
@@ -259,15 +327,22 @@ void ram_wr_dw(uint32_t adr, uint32_t val)
 #endif
 		return;
 	}
-	adr += 3;
+	//big endian:
+	/*adr += 3;
 	RAM[adr--] = val & 0xff;
 	RAM[adr--] = (val >>= 8) & 0xff;
 	RAM[adr--] = (val >>= 8) & 0xff;
-	RAM[adr]   = (val >>  8);
+	RAM[adr]   = (val >>  8);*/
+	val = __bswap_32(val);
+	memcpy(RAM+adr,&val,4);
+#endif /* SCACHE */
 }
 
 uint32_t ram_rd_dw(uint32_t adr)
 {
+#if RAM_SCACHE
+	return __bswap_32(ram_scache_r(adr));
+#else
 	uint32_t r = 0;
 	if (adr >= ramsize-3) {
 #ifdef RAM_DBG
@@ -278,11 +353,15 @@ uint32_t ram_rd_dw(uint32_t adr)
 #endif
 		return r;
 	}
-	r  = RAM[adr++]; r <<= 8;
+	//big endian:
+	/*r  = RAM[adr++]; r <<= 8;
 	r |= RAM[adr++]; r <<= 8;
 	r |= RAM[adr++]; r <<= 8;
 	r |= RAM[adr];
-	return r;
+	return r;*/
+	memcpy(&r,RAM+adr,4);
+	return __bswap_32(r);
+#endif /* SCACHE */
 }
 
 /*
