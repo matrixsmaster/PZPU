516077c6c1305c2a0f72bff5f4a5a7b5a14bee40
matrixsmaster <matrixsmaster@yandex.ru>
2019-08-26 23:05:48 +0200

    OISC CPU port: added simple Subleq interpreter

---
+++ test/subleq.c
@@ -0,0 +1,66 @@
+// Subleq experiment
+//(C) Dmitry 'MatrixS_Master' Solovyev, 2019
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+#define RAMSIZE 123000
+
+int main(int argc, char* argv[])
+{
+    assert(argc >= 2);
+
+    int step = ((argc > 2) && (!strcmp(argv[2],"-s")))? 1 : 0;
+
+    FILE* fi = fopen(argv[1],"r");
+    assert(fi);
+
+    int ram[RAMSIZE];
+    int val, ip = 0, maxip = 0;
+    while (fscanf(fi,"%d",&val) > 0) {
+        ram[ip++] = val;
+        assert(ip < RAMSIZE);
+    }
+    fclose(fi);
+    printf("Last val = %d, ip = %d\n",val,ip);
+
+    ip = 0;
+    int a,b,c;
+    size_t n = 0;
+    do {
+        a = ram[ip++];
+        b = ram[ip++];
+        c = ram[ip++];
+        assert(ip < RAMSIZE);
+
+        if (step) {
+            printf("N = %lu, IP = %d, Ra = %d, Rb = %d, Rc = %d, RRa = %d, RRb = %d\n",n,ip,a,b,c,ram[a],ram[b]);
+            getchar();
+        }
+
+        assert((a == -1 && b >= 0) || (b == -1 && a >= 0) || (a >= 0 && b >= 0));
+
+        if (a == -1) {
+            ram[b] += getchar();
+            if (ram[b] <= 0) ip = c;
+
+        } else if (b == -1) {
+            putchar(ram[a]);
+            fflush(stdout);
+
+        } else {
+            ram[b] -= ram[a];
+            if(ram[b] <= 0) ip = c;
+
+        }
+
+        if (ip > maxip) maxip = ip;
+        n++;
+    } while (ip >= 0);
+
+    printf("\nExecution complete. Top IP = %d\nDone.\n",maxip);
+    return 0;
+}
